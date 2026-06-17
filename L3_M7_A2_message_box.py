f7d5ae3103fa15648e387f73416125e0c08b994a
Divit Reddy Allu <lkuntla@googlemail.com>
2026-06-17 18:20:45 +0200

    Lesson 3 Module 7

---
+++ L3_M7_A2_message_box.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.geometry("200x200")
+
+def message():
+    messagebox.showwarning("Alert!", "Stop! Virus detected")
+button=Button(root,text="Scan for virus",command=message)
+button.place(x=40,y=80)
+root.mainloop()
